2760290c3765fc8a1b3920a36fb77e6be8dccce0
Rômulo Souza Fernandes <rromulofer@gmail.com>
2021-02-19 15:36:55 -0300

    Update 003 somando.py

--- Scripts-python/003 somando.py
+++ Scripts-python/003 somando.py
@@ -1,4 +1,4 @@
-n1 = int(input('Digite um numero: '))
-n2 = int(input('Digite outro numero: '))
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
 n3 = n1+n2
 print('{} + {} = {}'.format(n1, n2, n3))
